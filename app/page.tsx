45b314497faf7cbe1c8bf9c95718a9828e0cc0ed
MUKE-coder <gmukejohnbaptist@gmail.com>
2024-03-02 06:34:26 +0300

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
   return (
     <div className="px-16 py-16 bg-slate-50 min-h-screen">
       <h2 className="text-2xl py-6 font-bold text-center">
-        Form Validation with React Hook Forms and Zod in a Typescript Next js
-        Project
+        The Best way to Use Server Actions
       </h2>
 
       <div className="mx-auto w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
